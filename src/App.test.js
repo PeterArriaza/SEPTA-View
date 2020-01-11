35b9bc4cb07bdb9fdfc3ba4c304950f0ad5192c9
PeterArriaza <peter.arriaza@gmail.com>
2020-01-11 14:25:58 -0500

    wrapped app in Provider for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
 
 describe("<App />", () => {
   it("renders the app", () => {
-    shallow(<App />);
+    shallow(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
   });
 });
